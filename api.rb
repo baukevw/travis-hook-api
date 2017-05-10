e662fd68a95fa7a60c6baa740316e41b79350f8b
baukevw <bauke@baukevanwieren.nl>
2017-05-10 23:33:21 +0200

    logging did not really work...

--- api.rb
+++ api.rb
@@ -2,22 +2,13 @@ require 'sinatra'
 require 'json'
 require 'digest/sha2'
 require 'dotenv'
-require 'logger'
 
 # Setting the encoding
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-
 class TravisHookAPI < Sinatra::Base
   set :token, ENV['TRAVIS_USER_TOKEN']
-
-  configure do
-    LOGGER = Logger.new("sinatra.log")
-    enable :logging, :dump_errors
-    set :raise_errors, true
-  end
-
   Dotenv.load
 
   get '/' do
