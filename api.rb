ba61aeeaae9a362f8b20e83b4dfdf51ab3ab925f
baukevw <bauke@baukevanwieren.nl>
2017-05-11 17:15:10 +0200

    updated logger

--- api.rb
+++ api.rb
@@ -2,30 +2,45 @@ require 'sinatra'
 require 'json'
 require 'digest/sha2'
 require 'dotenv'
-require 'sinatra/logger'
+require 'logger'
 
 # Setting the encoding
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
+FILE = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+
+LOGGER = Logger.new(FILE)
+
 
 class TravisHookAPI < Sinatra::Base
   set :token, ENV['TRAVIS_USER_TOKEN']
-  logger filename: "log/#{settings.environment}.log", level: :trace
   Dotenv.load
 
+  configure do
+    enable :logging
+
+    FILE.sync = true
+    use Rack::CommonLogger, FILE
+
+    LOGGER.formatter = proc do |severity, datetime, progname, msg|
+       "LOG: #{msg}\n"
+    end
+  end
+
+
   get '/' do
-    logger.info("Hello World.")
+    LOGGER.info("Hello World")
     'Hello World'
   end
 
   post '/' do
     if not valid_request?
-      logger.info("Invalid payload request for repository #{repo_slug}")
+      LOGGER.info("Invalid payload request for repository #{repo_slug}")
     else
       payload = JSON.parse(params[:payload])
-      logger.info("Received valid payload for repository #{repo_slug}")
-      logger.info(payload)
+      LOGGER.info("Received valid payload for repository #{repo_slug}")
+      LOGGER.info(payload)
     end
   end
 
